42687e4b130b5a96fa26de1a2ef6ac5a216e2643
Antony Oduor <antouboduor@gmail.com>
2018-02-18 17:39:29 -0700

    Move X-Frame-Options Header; Append as Tuple

--- websauna/newsletter/adminviews.py
+++ websauna/newsletter/adminviews.py
@@ -73,10 +73,6 @@ class NewsletterSend(CSRFSchema):
 def newsletter(context: Admin, request: Request):
     """Newsletter admin form."""
     schema = NewsletterSend().bind(request=request)
-    headers = [
-        ('X-Frame-Options', 'SAMEORIGIN'),
-    ]
-    request.response.headerlist.extend(headers)
 
     # Create a styled button with some extra Bootstrap 3 CSS classes
     b = deform.Button(name='process', title="Send", css_class="btn-block btn-lg btn-primary")
@@ -138,6 +134,8 @@ def newsletter(context: Admin, request: Request):
     # if form widgets specify any static assets.
     form.resource_registry.pull_in_resources(request, form)
     
+    header = ('X-Frame-Options', 'SAMEORIGIN')
+    request.response.headerlist.append(header)
     return locals()
 
 
